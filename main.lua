a01ac849b1533e414c6ca5d8ca7b71f75b52fcf9
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-02 08:54:15 +0400

    Set larger default VantaUI window size

--- main.lua
+++ main.lua
@@ -241,6 +241,9 @@ function VantaUI:CreateWindow(config)
     if config.NewElements == nil then
         config.NewElements = true
     end
+    if config.Size == nil then
+        config.Size = UDim2.fromOffset(760, 520)
+    end
 
     local usesThemeWallpaper = config.Background == nil
     if usesThemeWallpaper then
